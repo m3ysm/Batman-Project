f1c1f5f0c08631f48af54424ad178198d4c7c4dc
meysam <meysam.mahdavi87@gmail.com>
2021-10-05 15:30:56 +0330

    base classes and some utils added.

---
+++ app/src/main/java/com/example/batmanproject/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.batmanproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.batmanproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/example/batmanproject/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/batmanproject/ui/base/BaseActivity.kt
@@ -1,9 +1,11 @@
-package com.example.batmanproject
+package com.example.batmanproject.ui.base
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.batmanproject.R
 
-class MainActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
